78ffea002b98290f352efb941c15500c0da4681e
Lipsa <lipsadas0710@gmail.com>
2025-06-18 01:29:50 +0530

    Landing page again

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -78,7 +78,7 @@ const HeroSection = () => {
         playsInline
         className="absolute top-0 left-0 w-full h-full object-cover"
       >
-        <source src="/video3.mp4" type="video/mp4" />
+        <source src="/video4.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
