e074aa6526b63aedde6e2458ab7a516b77d21f2a
Lipsa <lipsadas0710@gmail.com>
2025-06-22 01:56:42 +0530

    Added find venue button to booking

--- app/(userRoutes)/[username]/Profile/YourBooking/page.js
+++ app/(userRoutes)/[username]/Profile/YourBooking/page.js
@@ -1,10 +1,14 @@
 'use client';
 
 import { useEffect, useState } from "react";
+import { useContext } from "react";
+import { UserLocationContext } from '../../../../../context/UserlocationContext'
 
 export default function BookingPage() {
+  const {userLocation, setUserLocation} = useContext(UserLocationContext);
   const [bookings, setBookings] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [favourite, setFavourite] = useState([]);
 
   useEffect(() => {
     const fetchBookings = async () => {
@@ -27,6 +31,35 @@ export default function BookingPage() {
     fetchBookings();
   }, []);
 
+  useEffect(() => {
+    const fetchBookings = async () => {
+      try {
+        const res = await fetch("/api/favourite");
+        const data = await res.json();
+
+        if (data.success) {
+          setFavourite(data.favourite);
+        } else {
+          console.error("Error fetching favourite:", data.error);
+        }
+      } catch (error) {
+        console.error("API error:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchBookings();
+  }, []);
+
+   const onDirectionClick=()=>{
+      window.open('https://www.google.com/maps/dir/?api=1&origin='+
+      userLocation.lat+','+userLocation.lng+'&destination='
+      +favourite.lat
+      +','+favourite.lng+'&travelmode=driving');
+
+  }
+
   return (
     <div className="flex flex-col items-center justify-center mt-10 px-4 sm:px-6 lg:px-8">
       <h1 className="text-2xl font-bold mb-4 text-gray-700 dark:text-gray-200">Your Bookings</h1>
@@ -40,8 +73,9 @@ export default function BookingPage() {
           {bookings.map((booking, index) => (
             <div
               key={index}
-              className="border border-gray-200 dark:border-gray-700 rounded-xl p-4 bg-white dark:bg-gray-800 shadow-sm"
+              className="border border-gray-200 dark:border-gray-700 rounded-xl p-4 bg-white dark:bg-gray-800 shadow-sm flex justify-between"
             >
+              <div>
               <h2 className="text-xl font-semibold text-gray-800 dark:text-gray-100 mb-2">
                 {booking.restaurantName}
               </h2>
@@ -60,8 +94,11 @@ export default function BookingPage() {
                   <span className="font-medium">Special Request:</span> {booking.specialRequest}
                 </p>
               )}
+              </div>
+              <button className="bg-blue-500 text-white rounded-lg p-2 hover:bg-blue-700 h-10 " onClick={onDirectionClick}>Find Venue</button>
             </div>
           ))}
+          
         </div>
       )}
     </div>
